409538da128c1b1463a2e1752ad6524d83ad80ac
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-21 01:52:16 +0300

    Sign button statement added and code compacted

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -12,14 +12,46 @@ final class ViewController: UIViewController {
     @IBOutlet var numberLabel: UILabel!
     
     private var stringOfNumbers = ""
-    private var result = 0.0
+    private var resultOfCalculate = 0.0
     private var sign = ""
+    private var isSignTapped = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         numberLabel.text = "0"
     }
-
+    
+    @IBAction func tapButton(_ sender: UIButton) {
+        let numberOnButton = sender.titleLabel?.text
+        
+        if stringOfNumbers.count <= 8 {
+            switch numberOnButton {
+            case "0":
+                stringOfNumbers += "0"
+            case "1":
+                stringOfNumbers += "1"
+            case "2":
+                stringOfNumbers += "2"
+            case "3":
+                stringOfNumbers += "3"
+            case "4":
+                stringOfNumbers += "4"
+            case "5":
+                stringOfNumbers += "5"
+            case "6":
+                stringOfNumbers += "6"
+            case "7":
+                stringOfNumbers += "7"
+            case "8":
+                stringOfNumbers += "8"
+            default:
+                stringOfNumbers += "9"
+            }
+            
+            numberLabel.text = stringOfNumbers
+        }
+    }
+    
     @IBAction func tapDotButton(_ sender: UIButton) {
         if stringOfNumbers == "" {
             stringOfNumbers += "0."
@@ -30,135 +62,64 @@ final class ViewController: UIViewController {
         }
     }
     
-    @IBAction func tapZeroButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "0"
-            numberLabel.text = stringOfNumbers
+    @IBAction func tapSignButton(_ sender: UIButton) {
+        if !isSignTapped {
+            let signOnButton = sender.titleLabel?.text
+            
+            switch signOnButton {
+            case "+":
+                sign = "+"
+            case "-":
+                sign = "-"
+            case "x":
+                sign = "x"
+            default:
+                sign = "÷"
+            }
+            
+            resultOfCalculate = Double(numberLabel.text ?? "") ?? 0.0
+            stringOfNumbers = ""
         }
-    }
-    
-    @IBAction func tapOneButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "1"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapTwoButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "2"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    @IBAction func tapThreeButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "3"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapFourButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "4"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapFiveButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "5"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapSixButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "6"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapSevenButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "7"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapEightButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "8"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapNineButton(_ sender: UIButton) {
-        if stringOfNumbers.count <= 8 {
-            stringOfNumbers += "9"
-            numberLabel.text = stringOfNumbers
-        }
-    }
-    
-    @IBAction func tapPlusButton(_ sender: UIButton) {
-        result = Double(numberLabel.text ?? "") ?? 0.0
-        sign = "+"
-        stringOfNumbers = ""
-    }
-    
-    @IBAction func tapMinusButton(_ sender: UIButton) {
-        result = Double(numberLabel.text ?? "") ?? 0.0
-        sign = "-"
-        stringOfNumbers = ""
-    }
-    
-    @IBAction func tapMultiplyButton(_ sender: UIButton) {
-        result = Double(numberLabel.text ?? "") ?? 0.0
-        sign = "x"
-        stringOfNumbers = ""
-    }
-    
-    @IBAction func tapDivisionButton(_ sender: UIButton) {
-        result = Double(numberLabel.text ?? "") ?? 0.0
-        sign = "÷"
-        stringOfNumbers = ""
+        isSignTapped = true
     }
     
     @IBAction func tapResultButton(_ sender: UIButton) {
         switch sign {
         case "+":
-            result += Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculate += Double(numberLabel.text ?? "") ?? 0.0
         case "-":
-            result -= Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculate -= Double(numberLabel.text ?? "") ?? 0.0
         case "x":
-            result *= Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculate *= Double(numberLabel.text ?? "") ?? 0.0
         case "÷" where Double(numberLabel.text ?? "") ?? 0.0 == 0:
-            result = 0
+            resultOfCalculate = 0
             numberLabel.text = "Ошибка"
         case "÷":
-            result /= Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculate /= Double(numberLabel.text ?? "") ?? 0.0
         default:
-            result += 0
+            resultOfCalculate += 0
         }
         
         if numberLabel.text == "Ошибка" {
-            result = 0
-        } else if result.truncatingRemainder(dividingBy: 1) != 0 {
-            result = round(result * 100000000) / 100000000
-            numberLabel.text = String(result)
-        } else if String(result).count > 9 {
+            resultOfCalculate = 0
+        } else if resultOfCalculate.truncatingRemainder(dividingBy: 1) != 0 {
+            resultOfCalculate = round(resultOfCalculate * 100000000) / 100000000
+            numberLabel.text = String(resultOfCalculate)
+        } else if String(resultOfCalculate).count > 9 {
             numberLabel.text = "Большое число"
         } else {
-            let intResult = Int(result)
+            let intResult = Int(resultOfCalculate)
             numberLabel.text = String(intResult)
         }
         
-        sign = ""
         stringOfNumbers = ""
+        isSignTapped = false
     }
     
     @IBAction func tapClearButton(_ sender: UIButton) {
-        result = 0
+        resultOfCalculate = 0
         numberLabel.text = "0"
         stringOfNumbers = ""
+        isSignTapped = false
     }
 }
