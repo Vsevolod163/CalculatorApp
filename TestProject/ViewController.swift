c856a260662e630c0b65f8b83d9981a95d142c57
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-21 03:46:36 +0300

    Version 2.1.0 added (error statement added, currentTitle, new names for variables, code more compacted)

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -8,118 +8,95 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+    
     @IBOutlet var numberLabel: UILabel!
     
-    private var stringOfNumbers = ""
-    private var resultOfCalculate = 0.0
-    private var sign = ""
+    private var currentNumber = ""
+    private var resultOfCalculation = 0.0
+    private var signOfOperation = ""
     private var isSignTapped = false
+    private var isErrorAppear = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         numberLabel.text = "0"
     }
     
     @IBAction func tapButton(_ sender: UIButton) {
-        let numberOnButton = sender.titleLabel?.text
+        guard !isErrorAppear else { return }
+        guard currentNumber.count <= 8 else { return }
+        guard let number = sender.currentTitle else { return }
         
-        if stringOfNumbers.count <= 8 {
-            switch numberOnButton {
-            case "0":
-                stringOfNumbers += "0"
-            case "1":
-                stringOfNumbers += "1"
-            case "2":
-                stringOfNumbers += "2"
-            case "3":
-                stringOfNumbers += "3"
-            case "4":
-                stringOfNumbers += "4"
-            case "5":
-                stringOfNumbers += "5"
-            case "6":
-                stringOfNumbers += "6"
-            case "7":
-                stringOfNumbers += "7"
-            case "8":
-                stringOfNumbers += "8"
-            default:
-                stringOfNumbers += "9"
-            }
-            
-            numberLabel.text = stringOfNumbers
-        }
+        currentNumber += number
+        
+        numberLabel.text = currentNumber
     }
     
     @IBAction func tapDotButton(_ sender: UIButton) {
-        if stringOfNumbers == "" {
-            stringOfNumbers += "0."
-            numberLabel.text = stringOfNumbers
-        } else if !stringOfNumbers.contains(".") && stringOfNumbers.count <= 8 {
-            stringOfNumbers += "."
-            numberLabel.text = stringOfNumbers
+        if isErrorAppear {
+            numberLabel.text = "Ошибка"
+        } else if currentNumber == "" {
+            currentNumber += "0."
+            numberLabel.text = currentNumber
+        } else if !currentNumber.contains(".") && currentNumber.count <= 8 {
+            currentNumber += "."
+            numberLabel.text = currentNumber
         }
     }
     
     @IBAction func tapSignButton(_ sender: UIButton) {
-        if !isSignTapped {
-            let signOnButton = sender.titleLabel?.text
-            
-            switch signOnButton {
-            case "+":
-                sign = "+"
-            case "-":
-                sign = "-"
-            case "x":
-                sign = "x"
-            default:
-                sign = "÷"
-            }
-            
-            resultOfCalculate = Double(numberLabel.text ?? "") ?? 0.0
-            stringOfNumbers = ""
-        }
+        guard let operation = sender.currentTitle else { return }
+        guard !isSignTapped else { return }
+        
+        signOfOperation = operation
+        resultOfCalculation = Double(numberLabel.text ?? "") ?? 0.0
+        currentNumber = ""
         isSignTapped = true
     }
     
     @IBAction func tapResultButton(_ sender: UIButton) {
-        switch sign {
+        guard !isErrorAppear else { return }
+        
+        switch signOfOperation {
         case "+":
-            resultOfCalculate += Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculation += Double(numberLabel.text ?? "") ?? 0.0
         case "-":
-            resultOfCalculate -= Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculation -= Double(numberLabel.text ?? "") ?? 0.0
         case "x":
-            resultOfCalculate *= Double(numberLabel.text ?? "") ?? 0.0
+            resultOfCalculation *= Double(numberLabel.text ?? "") ?? 0.0
         case "÷" where Double(numberLabel.text ?? "") ?? 0.0 == 0:
-            resultOfCalculate = 0
-            numberLabel.text = "Ошибка"
-        case "÷":
-            resultOfCalculate /= Double(numberLabel.text ?? "") ?? 0.0
+            isErrorAppear = true
         default:
-            resultOfCalculate += 0
+            resultOfCalculation /= Double(numberLabel.text ?? "") ?? 0.0
         }
         
-        if numberLabel.text == "Ошибка" {
-            resultOfCalculate = 0
-        } else if resultOfCalculate.truncatingRemainder(dividingBy: 1) != 0 {
-            resultOfCalculate = round(resultOfCalculate * 100000000) / 100000000
-            numberLabel.text = String(resultOfCalculate)
-        } else if String(resultOfCalculate).count > 9 {
-            numberLabel.text = "Большое число"
-        } else {
-            let intResult = Int(resultOfCalculate)
-            numberLabel.text = String(intResult)
-        }
+        getResultOfCalculation()
         
-        stringOfNumbers = ""
+        currentNumber = ""
         isSignTapped = false
     }
     
     @IBAction func tapClearButton(_ sender: UIButton) {
-        resultOfCalculate = 0
+        resultOfCalculation = 0
         numberLabel.text = "0"
-        stringOfNumbers = ""
+        currentNumber = ""
         isSignTapped = false
+        isErrorAppear = false
+    }
+    
+    private func getResultOfCalculation() {
+        if isErrorAppear {
+            numberLabel.text = "Ошибка"
+        } else if String(resultOfCalculation).count > 9 {
+            numberLabel.text = "Большое число"
+            isErrorAppear = true
+        } else if resultOfCalculation.truncatingRemainder(dividingBy: 1) != 0 {
+            resultOfCalculation = round(resultOfCalculation * 100000000) / 100000000
+            numberLabel.text = String(resultOfCalculation)
+        } else {
+            let intResult = Int(resultOfCalculation)
+            numberLabel.text = String(intResult)
+        }
     }
 }
