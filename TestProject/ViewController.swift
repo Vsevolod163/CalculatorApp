5e428cc6ca5e494affdf6bb22861dc147d860870
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-21 20:00:02 +0300

    Bugs fixed with result button, private to functions added

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    @IBOutlet var numberLabel: UILabel!
+    @IBOutlet private var numberLabel: UILabel!
     
     private var currentNumber = ""
     private var resultOfCalculation = 0.0
@@ -23,7 +23,7 @@ final class ViewController: UIViewController {
         numberLabel.text = "0"
     }
     
-    @IBAction func tapButton(_ sender: UIButton) {
+    @IBAction private func tapNumberButton(_ sender: UIButton) {
         guard !isErrorAppear else { return }
         guard currentNumber.count <= 8 else { return }
         guard let number = sender.currentTitle else { return }
@@ -33,10 +33,10 @@ final class ViewController: UIViewController {
         numberLabel.text = currentNumber
     }
     
-    @IBAction func tapDotButton(_ sender: UIButton) {
-        if isErrorAppear {
-            numberLabel.text = "Ошибка"
-        } else if currentNumber == "" {
+    @IBAction private func tapDotButton(_ sender: UIButton) {
+        guard !isErrorAppear else { return }
+        
+        if currentNumber == "" {
             currentNumber += "0."
             numberLabel.text = currentNumber
         } else if !currentNumber.contains(".") && currentNumber.count <= 8 {
@@ -45,7 +45,7 @@ final class ViewController: UIViewController {
         }
     }
     
-    @IBAction func tapSignButton(_ sender: UIButton) {
+    @IBAction private func tapSignButton(_ sender: UIButton) {
         guard let operation = sender.currentTitle else { return }
         guard !isSignTapped else { return }
         
@@ -55,7 +55,7 @@ final class ViewController: UIViewController {
         isSignTapped = true
     }
     
-    @IBAction func tapResultButton(_ sender: UIButton) {
+    @IBAction private func tapResultButton(_ sender: UIButton) {
         guard !isErrorAppear else { return }
         
         switch signOfOperation {
@@ -67,20 +67,25 @@ final class ViewController: UIViewController {
             resultOfCalculation *= Double(numberLabel.text ?? "") ?? 0.0
         case "÷" where Double(numberLabel.text ?? "") ?? 0.0 == 0:
             isErrorAppear = true
-        default:
+        case "÷":
             resultOfCalculation /= Double(numberLabel.text ?? "") ?? 0.0
+        default:
+            break
         }
         
-        getResultOfCalculation()
+        if signOfOperation != "" {
+            getResultOfCalculation()
+        }
         
+        signOfOperation = ""
         currentNumber = ""
         isSignTapped = false
     }
     
-    @IBAction func tapClearButton(_ sender: UIButton) {
-        resultOfCalculation = 0
+    @IBAction private func tapClearButton(_ sender: UIButton) {
         numberLabel.text = "0"
         currentNumber = ""
+        signOfOperation = ""
         isSignTapped = false
         isErrorAppear = false
     }
@@ -88,12 +93,12 @@ final class ViewController: UIViewController {
     private func getResultOfCalculation() {
         if isErrorAppear {
             numberLabel.text = "Ошибка"
-        } else if String(resultOfCalculation).count > 9 {
-            numberLabel.text = "Большое число"
-            isErrorAppear = true
         } else if resultOfCalculation.truncatingRemainder(dividingBy: 1) != 0 {
             resultOfCalculation = round(resultOfCalculation * 100000000) / 100000000
             numberLabel.text = String(resultOfCalculation)
+        } else if String(resultOfCalculation).count > 9 {
+            numberLabel.text = "Большое число"
+            isErrorAppear = true
         } else {
             let intResult = Int(resultOfCalculation)
             numberLabel.text = String(intResult)
